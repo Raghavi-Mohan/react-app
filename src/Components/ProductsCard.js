bfd7653d4a51d93d9d6b4bcd26002d1185800f8e
Raghavi Mohan <70186244+Raghavi-Mohan@users.noreply.github.com>
2025-05-20 19:38:23 -0400

    mobile/web browser commit

--- src/Components/ProductsCard.js
+++ src/Components/ProductsCard.js
@@ -5,7 +5,7 @@ export default function ProductsCard({ topic, size, children }) {
     return (
         <Fragment>
             <div className="Mycard w-25 m-4 d-inline-block">
-                <div className="card-header text-center">
+                <div className="card-header w-75 text-center">
                     <h5>{topic}</h5>
                 </div>
                 <div className="Mycard-body zoom-hover">
