73eaf9a874e9023614a5bbdf7f9a609734950465
Raghavi Mohan <70186244+Raghavi-Mohan@users.noreply.github.com>
2025-06-04 21:47:45 -0400

    new paintings  commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -24,7 +24,7 @@ export default class Home extends Component {
        </ContentBlockleftImg>
        <Headings  heading="Meet The Artist"/>
        <ContentBlock text="Hi there, I’m Raghavi!!" buttonText="About Me" src={me} route="aboutme">
-       <p className="text-center w-50 ms-1">I am a self representing abstract artist from my home studio just outside of Naples, Florida. My artistic process is driven by spontaneity and layered techniques. I work primarily with acrylics, mixed media, and textural elements to build atmospheric compositions that evoke both movement and stillness. From textured canvases to sculptural forms, I aim to create art that invites the viewer to pause, interpret, and connect</p>
+       <p className="text-center w-75 ms-1">I am a self representing abstract artist from my home studio just outside of Naples, Florida. My artistic process is driven by spontaneity and layered techniques. I create artwork using acrylics and textures to show peaceful and lively moments together.</p>
        </ContentBlock>
        <Footer />
       </div>
